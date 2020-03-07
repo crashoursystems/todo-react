0c556209e9171aac695a6207b1486d818a6f709e
crashoursystems <killoursustems@gmail.com>
2020-03-07 12:44:31 +0400

    Readme add

---
+++ src/components/todo-add-item/todo-add-item.js
@@ -0,0 +1,16 @@
+import React from "react";
+import 'todo-add-item.css';
+
+const TodoAddItem = () => {
+
+    return (
+        <div>
+            <button type="button"
+                    className="btn btn-success">
+
+             </button>
+        </div>
+    );
+}
+
+export default TodoAddItem;
